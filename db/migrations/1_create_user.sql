3b6493896d63ce6579385b87e78b232fc80b0e83
reececilon <reecethat@gmail.com>
2022-07-18 10:40:36 +0100

    initial commit : set up basic file structure and started tables

---
+++ db/migrations/1_create_user.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    username varchar(200) NOT NULL,
+    email varchar(500) NOT NULL,
+    password varchar(200), -- (maybe hashed?)
+);

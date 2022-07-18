796af9a22c4ed0706bb29a6b25e8de4813f1473e
Ismael Ali <ismaelali78@outlook.com>
2022-07-18 15:25:07 +0100

    Added authentication

--- api/models/User.js
+++ api/models/User.js
@@ -1,27 +1,53 @@
-const db = require('../dbConfig/init');
+const db = require("../dbConfig/init");
 
 class User {
-    constructor(data){
-        this.id = data.id
-        this.username = data.username
-        this.email = data.email
-        this.password = data.password
-    }
+  constructor(data) {
+    this.id = data.id;
+    this.username = data.username;
+    this.email = data.email;
+    this.password = data.password;
+  }
 
-    static create(username, email, password) {
-        return new Promise(async (resolve, reject) => {
-          try {
-            let userData = await db.query(
-              `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *;`,
-              [username, email, password]
-            );
-            let newUser = new User(userData.rows[0]);
-            resolve(newUser);
-          } catch (err) {
-            reject("Error creating post");
-          }
-        });
+  static get all() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let userData = await db.query("SELECT * FROM users");
+        let users = userData.rows.map((user) => new User(user));
+        resolve(users);
+      } catch (err) {
+        reject("Error Retrieving Users");
       }
+    });
+  }
+
+  static create(username, email, password) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let userData = await db.query(
+          `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *;`,
+          [username, email, password]
+        );
+        let newUser = new User(userData.rows[0]);
+        resolve(newUser);
+      } catch (err) {
+        reject("Error Registering User");
+      }
+    });
+  }
+
+  static findUser(email) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let userData = await db.query("SELECT * FROM users WHERE email = $1", [
+          email,
+        ]);
+        let selectedUser = new User(userData.rows[0]);
+        resolve(selectedUser);
+      } catch (err) {
+        reject("Error User Does Not Exist");
+      }
+    });
+  }
 }
 
 module.exports = User;
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/users");
 
-router.post("/", usersController.create);
+router.post("/register", usersController.register);
+router.post("/login", usersController.login);
+router.get("/", usersController.showUsers);
 
 module.exports = router;

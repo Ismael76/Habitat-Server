8b71e34c0e77a77e0cea32d704e81df7fad4ebc6
Ismael Ali <ismaelali78@outlook.com>
2022-07-19 10:38:28 +0100

    fixed all issues with routing

--- api/controllers/habits.js
+++ api/controllers/habits.js
@@ -4,21 +4,22 @@ async function createHabit(req, res) {
   try {
     //From Front End We Want To Send Frequency, Title Of Habit From The Form & The Email Of Logged In User
     const { frequency, title, email } = req.body;
-    const habit = await User.create(frequency, title, email);
+    const habit = await Habit.create(frequency, title, email);
     res.status(201).json(habit);
   } catch (err) {
     res.status(422).json({ err });
   }
 }
 
-// async function showHabits(req, res) {
-//   try {
-//     const { frequency, title } = req.body;
-//     const habit = await User.create(frequency, title);
-//     res.status(201).json(habit);
-//   } catch (err) {
-//     res.status(422).json({ err });
-//   }
-// }
+async function showHabits(req, res) {
+  try {
+    console.log("IN HERE!");
+    const habit = await Habit.all;
+    console.log(habit);
+    res.status(201).json(habit);
+  } catch (err) {
+    res.status(422).json({ err });
+  }
+}
 
-module.exports = { createHabit };
+module.exports = { createHabit, showHabits };

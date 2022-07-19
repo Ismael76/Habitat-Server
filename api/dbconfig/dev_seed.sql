d79f2c4d3d1c90390e22bf2bc43c83d5b826278c
daiana.dobre@getfutureproo.co.uk <daiana_melania@yahoo.com>
2022-07-19 16:14:43 +0100

    insert into tracker

--- api/dbconfig/dev_seed.sql
+++ api/dbconfig/dev_seed.sql
@@ -12,3 +12,10 @@ VALUES
     ('Go Jogging', 2, 1),
     ('Drink Water', 8, 1),
     ('Go Swimming', 1, 3)
+
+INSERT INTO tracker (date, progress, status, habit_id) 
+VALUES
+    ('12-06-2020', 5, true, 1),
+    ('14-02-2021', 2, false, 1),
+    ('13-02-2022', 8, true, 1),
+    ('13-02-2022', 1, true, 3)
--- db/migrations/2_create_habits.sql
+++ db/migrations/2_create_habits.sql
@@ -4,6 +4,5 @@ CREATE TABLE habits (
     id serial PRIMARY KEY,
     title varchar(200) NOT NULL,
     frequency INT NOT NULL,
-    user_id INT REFERENCES users (id),
-    status BOOLEAN NOT NULL DEFAULT 0
+    user_id INT REFERENCES users (id)
 );

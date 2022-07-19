da4639cc009913e5cb9b8e47162ae545953af476
daiana.dobre@getfutureproo.co.uk <daiana_melania@yahoo.com>
2022-07-19 14:29:21 +0100

    trcaker table modified

--- db/migrations/3_create_tracker.sql
+++ db/migrations/3_create_tracker.sql
@@ -2,9 +2,9 @@ DROP TABLE IF EXISTS tracker;
 
 CREATE TABLE tracker (
     id serial PRIMARY KEY,
-    date DATE NOT NULL,
-    progress INT NOT NULL,
-    status boolean NOT NULL,
-    reward boolean NOT NULL,
+    date DATE DEFAULT CURRENT_DATE,
+    progress INT NOT NULL DEFAULT 0,
+    status boolean NOT NULL DEFAULT 0,
+    reward boolean NOT NULL DEFAULT 0,
     habit_id INT REFERENCES habits (id)
 );

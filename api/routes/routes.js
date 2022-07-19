5ac7cb41aec991592a6a4ac3d5f122be9834252b
daiana.dobre@getfutureproo.co.uk <daiana_melania@yahoo.com>
2022-07-19 16:16:16 +0100

    route added for completed habbits

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/users");
 const habitsController = require("../controllers/habits");
+const { route } = require("../server");
 
 
 router.post("/register", usersController.register);
@@ -9,6 +10,9 @@ router.post("/login", usersController.login);
 router.get("/", usersController.showUsers);
 
 router.get("/habits", habitsController.showHabits);
+router.get("/habitsCompleted", habitsController.showCompletedHabits);
 router.post("/createHabit", habitsController.createHabit);
+
+
 
 module.exports = router;

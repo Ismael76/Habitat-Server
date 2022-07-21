4514b7a8075ddbd0ce36f27b23c47bd13cb2021d
Ismael Ali <ismaelali78@outlook.com>
2022-07-21 13:07:38 +0100

    removed comments

--- api/scheduleTask.js
+++ api/scheduleTask.js
@@ -4,14 +4,12 @@ const Habit = require("./models/Habit");
 async function checkCompleted() {
   let allHabits = await Habit.all;
 
-  console.log("IN THIS CHECK COMPLETED FUNCTION");
   allHabits.forEach(async (habit) => {
     await resetHabit(habit);
   });
 }
 
 async function resetHabit(habit) {
-  console.log("IN THIS RESET HABIT FUNCTION");
   let obj = { completed: "f" };
   if (habit.completed == true) {
     try {
